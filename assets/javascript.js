28624825ef74e0a87bb1658e2c91ce6c4f35554c
Eshaffer <edmond.shaffer@gmail.com>
2018-10-17 18:34:25 -0400

    I broke my portfolio

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,23 @@
-$(document).ready(function(){
+var bio = "Welcome to the amazing portfolio of the nerdy Full Stack developer known as Mondo";
+var bioArry = bio.split("");
+var loopTime;
+function frameLooper() {
+    if (bioArry.length > 0) {
+        document.getElementById("bio_text").innerHTML += bioArry.shift();
+    } else {
+        clearTimeout(loopTime);
+        return false;
+    }
+    loopTime = setTimeout("frameLooper()", 70);
+}
+frameLooper();
+
+
+
+$(document).ready(function () {
+
+
+
     $(".collapsible").collapsible();
     // console.log ("hello");
 
@@ -8,23 +27,20 @@ $(document).ready(function(){
 
     $(".pic").addClass("animated slideInLeft");
 
-    $(".bio").addClass("animated fadeInRight");
-    
+    // $(".bio").addClass("animated fadeInRight");
+
     $(".carousel").addClass("animated fadeInUp");
 
     $(".rowOne").addClass("animated zoomIn");
 
-    $(".card-image").on("click", function(){
+    $(".card-image").on("click", function () {
         alert("you're about to leave this page.")
-  
+
     });
 
-//  setTimeout (function(){
-//     $(".rowOne").addClass("animated zoomIn").removeClass("hide");
-//     }, 400);
+    M.AutoInit();
 
 
-    
 
-  });
-        
+
+});
