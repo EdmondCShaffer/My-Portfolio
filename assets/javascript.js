cec710f9391016c8d9977505c82d7c5f13a1b625
Eshaffer <edmond.shaffer@gmail.com>
2018-10-26 19:28:59 -0400

    Changes to all pages half way done

--- assets/javascript.js
+++ assets/javascript.js
@@ -10,12 +10,44 @@ function frameLooper() {
     }
     loopTime = setTimeout("frameLooper()", 70);
 }
-frameLooper();
 
+let i = 0;
+let images = ["assets/images/image1.jpg","assets/images/image2.jpg"];
+let time = 5000;
+
+
+
+const changeImg = () => {
+    document.slide.src = images[i];
+    
+    if(i < images.length - 1){
+        i++
+    }else{
+        i = 0;
+    }
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg
+
+let j = 0;
+let bgImages = ["assets/images/BackgroundOne.jpg","assets/images/BackgroundTwo.jpg"];
+let bgTime = 20000;
+
+const changeBgImg = () =>{
+    document.slideBg.src = bgImages[j];
+    if(j < bgImages.length -1){
+        j++
+    }else{
+        j = 0;
+    }
+    setTimeout("changeBgImg()",bgTime);
+}
+changeBgImg();
 
 
 $(document).ready(function () {
-
+   
 
 
     $(".collapsible").collapsible();
